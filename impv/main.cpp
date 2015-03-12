77ec50621395844b081a838937c0fcb2116932b5
lishiwang <wanglishi1@gmail.com>
2015-03-12 16:21:12 -0500

    Implied volatility

--- impv/main.cpp
+++ impv/main.cpp
@@ -5,13 +5,29 @@
 #include "interval_bisection.h"
 #include <iostream>
 
+using namespace std;
+
 int main(int argc, char **argv) {
 	// First we create the parameter list
-	double S = 50;  // Underlying spot price
-	double K = 50;  // Strike price
-	double r = 0.05;   // Risk-free rate (5%)
-	double T = 1.0;    // One year until expiry
-	double C_M = 5; // Option market price
+	double F;  // Underlying future price
+	double K;  // Strike price
+	double r;   // Risk-free rate (5%)
+	double T;    // One year until expiry
+	double C_M; // Option market price
+
+	cout << "Please enter underlying future price: ";
+	cin >> F;
+	cout << "Please enter strike price: ";
+	cin >> K;
+	cout << "Please enter risk_free interest rate: ";
+	cin >> r;
+	cout << "Please enter expiration time of option: ";
+	cin >> T;
+	cout << "Please enter option market prcie: ";
+	cin >> C_M;
+
+
+	double S = F*exp(-r*T);//using Black's model, the underliying asset price should be discounted future price 
 
 	// Create the Black-Scholes Call functor
 	BlackScholesCall bsc(S, K, r, T);
